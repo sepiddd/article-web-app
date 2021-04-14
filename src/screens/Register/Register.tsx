8f0c52c5cd79e1fdb2d06e0a0c1972623b00aabd
sepide khadempir <sepide.khadempir@gmail.com>
2021-04-14 19:24:13 +0430

    add Register Screen

---
+++ src/screens/Register/Register.tsx
@@ -0,0 +1,57 @@
+import { Input, Button, Form } from "antd";
+
+const layout = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 16 },
+};
+const tailLayout = {
+  wrapperCol: { offset: 8, span: 16 },
+};
+
+const Register = () => {
+  const onFinish = (values: any) => {
+    console.log("Success:", values);
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  return (
+    <Form
+      {...layout}
+      name='basic'
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}>
+      <Form.Item
+        label='First Name'
+        name='firstname'
+        rules={[{ required: true, message: "Please input your username!" }]}>
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label='Username'
+        name='username'
+        rules={[{ required: true, message: "Please input your username!" }]}>
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label='Password'
+        name='password'
+        rules={[{ required: true, message: "Please input your password!" }]}>
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item {...tailLayout}>
+        <Button type='primary' htmlType='submit'>
+          Register
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default Register;
