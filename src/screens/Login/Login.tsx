ff9214c8b9c21ad59feb00488c1b8c0b77f6e65f
sepide khadempir <sepide.khadempir@gmail.com>
2021-04-20 23:16:15 +0430

    add routes to login and register pages

--- src/screens/Login/Login.tsx
+++ src/screens/Login/Login.tsx
@@ -1,7 +1,8 @@
-import { Input, Button, Form, notification } from "antd";
+import { Input, Button, Form, notification, Space } from "antd";
 import { useHistory } from "react-router";
 import { useArticle, useAuth } from "../../hooks";
-import { PATH_APP } from "../../routes/paths";
+import { Link } from "react-router-dom";
+import { PATH_APP, PATH_AUTH } from "../../routes/paths";
 
 const layout = {
   labelCol: { span: 8 },
@@ -37,31 +38,38 @@ const Login = () => {
   };
 
   return (
-    <Form
-      {...layout}
-      name='login'
-      initialValues={{ remember: true }}
-      onFinish={handleSubmit}>
-      <Form.Item
-        label='Email'
-        name='email'
-        rules={[{ required: true, message: "Please input your username!" }]}>
-        <Input />
-      </Form.Item>
+    <>
+      <Form
+        {...layout}
+        name='login'
+        initialValues={{ remember: true }}
+        onFinish={handleSubmit}>
+        <Form.Item
+          label='Email'
+          name='email'
+          rules={[{ required: true, message: "Please input your username!" }]}>
+          <Input />
+        </Form.Item>
 
-      <Form.Item
-        label='Password'
-        name='password'
-        rules={[{ required: true, message: "Please input your password!" }]}>
-        <Input.Password />
-      </Form.Item>
+        <Form.Item
+          label='Password'
+          name='password'
+          rules={[{ required: true, message: "Please input your password!" }]}>
+          <Input.Password />
+        </Form.Item>
 
-      <Form.Item {...tailLayout}>
-        <Button type='primary' htmlType='submit'>
-          Login
-        </Button>
-      </Form.Item>
-    </Form>
+        <Form.Item {...tailLayout}>
+          <Button type='primary' htmlType='submit'>
+            Login
+          </Button>
+        </Form.Item>
+      </Form>
+      <Space align='center' style={{ justifyContent: "center", width: "100%" }}>
+        <p>
+          Dont Have an acoount? <Link to={PATH_AUTH.register}>register</Link>
+        </p>
+      </Space>
+    </>
   );
 };
 
--- src/screens/Register/Register.tsx
+++ src/screens/Register/Register.tsx
@@ -1,17 +1,10 @@
-import { Input, Button, Form, notification } from "antd";
+import { Input, Button, Form, notification, Space } from "antd";
 import { useHistory } from "react-router";
 import { useAuth } from "../../hooks";
 import { User } from "../../types";
 import { Controller, useForm } from "react-hook-form";
-// import { yupResolver } from "@hookform/resolvers/yup";
-// import * as yup from "yup";
-
-// const schema = yup.object().shape({
-//   firstName: yup.string().required().min(3).max(50),
-//   lastName: yup.string().required().min(3).max(50),
-//   email: yup.string().email().required(),
-//   password: yup.string().required().min(8),
-// });
+import { PATH_AUTH } from "../../routes/paths";
+import { Link } from "react-router-dom";
 
 const layout = {
   labelCol: { span: 8 },
@@ -43,15 +36,38 @@ const Register = () => {
   };
 
   return (
-    <Form {...layout} name='register' onFinish={handleSubmit(onSubmit)}>
-      <Controller
-        render={({ field }) => {
-          return (
+    <>
+      <Form {...layout} name='register' onFinish={handleSubmit(onSubmit)}>
+        <Controller
+          render={({ field }) => {
+            return (
+              <Form.Item
+                name={field.name}
+                label='First Name'
+                rules={[
+                  { required: true, message: "Please input your firstname!" },
+                  { min: 3, message: "min length!" },
+                  { max: 50, message: "max length!" },
+                ]}>
+                <Input
+                  onChange={field.onChange}
+                  value={field.value}
+                  onBlur={field.onBlur}
+                />
+              </Form.Item>
+            );
+          }}
+          name='firstName'
+          control={control}
+        />
+
+        <Controller
+          render={({ field }) => (
             <Form.Item
               name={field.name}
-              label='First Name'
+              label='Last Name'
               rules={[
-                { required: true, message: "Please input your firstname!" },
+                { required: true, message: "Please input your lastname!" },
                 { min: 3, message: "min length!" },
                 { max: 50, message: "max length!" },
               ]}>
@@ -61,84 +77,68 @@ const Register = () => {
                 onBlur={field.onBlur}
               />
             </Form.Item>
-          );
-        }}
-        name='firstName'
-        control={control}
-      />
+          )}
+          name='lastName'
+          control={control}
+        />
 
-      <Controller
-        render={({ field }) => (
-          <Form.Item
-            name={field.name}
-            label='Last Name'
-            rules={[
-              { required: true, message: "Please input your lastname!" },
-              { min: 3, message: "min length!" },
-              { max: 50, message: "max length!" },
-            ]}>
-            <Input
-              onChange={field.onChange}
-              value={field.value}
-              onBlur={field.onBlur}
-            />
-          </Form.Item>
-        )}
-        name='lastName'
-        control={control}
-      />
+        <Controller
+          render={({ field }) => {
+            return (
+              <Form.Item
+                name={field.name}
+                label='Email Address'
+                rules={[
+                  { required: true, message: "Please input your email!" },
+                  {
+                    pattern: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                    message: "your email address is not  valid",
+                  },
+                ]}>
+                <Input
+                  onChange={field.onChange}
+                  value={field.value}
+                  onBlur={field.onBlur}
+                />
+              </Form.Item>
+            );
+          }}
+          name='email'
+          control={control}
+        />
 
-      <Controller
-        render={({ field }) => {
-          return (
+        <Controller
+          render={({ field }) => (
             <Form.Item
               name={field.name}
-              label='Email Address'
+              label='Password'
               rules={[
-                { required: true, message: "Please input your email!" },
-                {
-                  pattern: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                  message: "your email address is not  valid",
-                },
+                { required: true, message: "Please input your password!" },
+                { min: 8, message: "Please input your password!" },
               ]}>
-              <Input
+              <Input.Password
                 onChange={field.onChange}
                 value={field.value}
                 onBlur={field.onBlur}
               />
             </Form.Item>
-          );
-        }}
-        name='email'
-        control={control}
-      />
+          )}
+          name='password'
+          control={control}
+        />
 
-      <Controller
-        render={({ field }) => (
-          <Form.Item
-            name={field.name}
-            label='Password'
-            rules={[
-              { required: true, message: "Please input your password!" },
-              { min: 8, message: "Please input your password!" },
-            ]}>
-            <Input.Password
-              onChange={field.onChange}
-              value={field.value}
-              onBlur={field.onBlur}
-            />
-          </Form.Item>
-        )}
-        name='password'
-        control={control}
-      />
-
-      <Form.Item {...tailLayout}>
-        <Button type='primary' htmlType='submit'>
-          Register
-        </Button>
-      </Form.Item>
-    </Form>
+        <Form.Item {...tailLayout}>
+          <Button type='primary' htmlType='submit'>
+            Register
+          </Button>
+        </Form.Item>
+      </Form>
+      <Space align='center' style={{ justifyContent: "center", width: "100%" }}>
+        <p>
+          Already Have an acoount? <Link to={PATH_AUTH.login}>Login</Link>
+        </p>
+      </Space>
+    </>
   );
 };
 
