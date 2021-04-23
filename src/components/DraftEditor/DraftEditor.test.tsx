4bcde0330c83e8b980b04371eb2ed4147e4d2e9a
sepide khadempir <sepide.khadempir@gmail.com>
2021-04-23 22:08:34 +0430

    add test for DraftEditor

---
+++ src/components/DraftEditor/DraftEditor.test.tsx
@@ -0,0 +1,27 @@
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+import DraftEditor from "./DraftEditor";
+
+let container: any = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+// mode==="add"
+it("Should render the component in add mode", () => {
+  act(() => {
+    render(<DraftEditor mode='add' />, container);
+  });
+
+  //   expect(container.querySelector("[data-testid='remove-img']")).toBe(null);
+  expect(container.textContent).toBe("Content...");
+});
